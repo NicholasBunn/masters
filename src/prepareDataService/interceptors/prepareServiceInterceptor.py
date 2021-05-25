be93ac3f56ec5804c322a6bc96c2f932d128e063
Nicholas Bunn <nicholasbunn04@gmail.com>
2021-05-25 14:38:16 +0200

    Standardised Python metric semantics

--- src/prepareDataService/interceptors/prepareServiceInterceptor.py
+++ src/prepareDataService/interceptors/prepareServiceInterceptor.py
@@ -10,10 +10,10 @@ import prometheus_client as prometheus
 logger = logging.getLogger(__file__.rsplit("/")[-3].rsplit(".")[0])
 logger.setLevel(logging.DEBUG)
 
-def pushToPrometheus(c, g, h, executionTime, address, job, registry):
-	c.inc()
-	g.set_to_current_time()
-	h.observe(executionTime)
+def pushToPrometheus(c, g, h, executionTime, serviceName, address, job, registry):
+	c.labels(grpc_type = 'unary', grpc_service = serviceName, grpc_method = job).inc()
+	g.labels(grpc_type = 'unary', grpc_service = serviceName, grpc_method = job).set_to_current_time()
+	h.labels(grpc_type = 'unary', grpc_service = serviceName, grpc_method = job).observe(executionTime)
 	
 	prometheus.push_to_gateway(address, job=job, registry=registry)
 	logger.info("Succesfully pushed metrics")
@@ -42,7 +42,7 @@ def sendMetrics(func):
 			raise
 		finally:
 			responseTime = time.time() - startTime
-			pushToPrometheus(args[0].c, args[0].g, args[0].h, responseTime, args[0].address, methodName, args[0].registry)
+			pushToPrometheus(args[0].c, args[0].g, args[0].h, responseTime, serviceName.rsplit(".")[1], args[0].address, methodName, args[0].registry)
 		return result
 	return wrapper
 
@@ -53,9 +53,9 @@ class MetricInterceptor(ServerInterceptor):
 	def __init__(self):
 		logger.debug("Initialising metric interceptor")
 		self.registry = prometheus.CollectorRegistry()
-		self.c = prometheus.Counter("server_request_counter", "Number of times this API has been called", registry=self.registry)
-		self.g = prometheus.Gauge('server_last_call_time', 'Last time this API was called', registry=self.registry)
-		self.h = prometheus.Histogram('server_request_latency', 'Ammount of time for request to be processed', registry=self.registry)
+		self.c = prometheus.Counter("server_request_counter", "Number of times this API has been called", registry=self.registry, labelnames= ['grpc_type', 'grpc_service', 'grpc_method'])
+		self.g = prometheus.Gauge('server_last_call_time', 'Last time this API was called', registry=self.registry, labelnames= ['grpc_type', 'grpc_service', 'grpc_method'])
+		self.h = prometheus.Histogram('server_request_latency', 'Ammount of time for request to be processed', registry=self.registry, labelnames= ['grpc_type', 'grpc_service', 'grpc_method'])
 
 	@sendMetrics
 	def intercept(self, method, request, context, methodName):
